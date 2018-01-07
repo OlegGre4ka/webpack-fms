39ef271be2c282011b904d60ba11b9c1cce0ed0c
OlegGre4ka <gre4kae@ukr.net>
2018-01-07 10:57:17 +0300

    npm init git init npx webpack

---
+++ init.js
@@ -0,0 +1,6 @@
+import './a.js';
+import './b.js';
+//Урок2.Установка і перший запуск webpack
+//Розробники радять установлювати локально щоб не було плутанини версій npm install --save-dev webpack
+//і тоді призапуску потрібно використовувати команду npx webpack init.js out.js
+console.log('./init')
